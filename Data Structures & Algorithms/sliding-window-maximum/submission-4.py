cd15e60e392c62e0fe7a9db256645a3e466dbef0
ktiwari8760 <110582982+ktiwari8760@users.noreply.github.com>
2026-05-24 16:10:32 +0530

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque()
+        l , r = 0 , 0
+        output = []
+        while r < len(nums):
+            while dq and nums[dq[-1]] < nums[r]: # check from back not front
+                dq.pop() # we remove the last element
+            dq.append(r)
+            # The element becomes useless here because 
+            if l > dq[0]:
+                dq.popleft()
+            
+            if (r+1) >= k:
+                output.append(nums[dq[0]])
+                l += 1
+            r += 1
+        return output
+
+            
+
+                
