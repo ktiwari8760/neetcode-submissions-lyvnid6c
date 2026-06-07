96fb62a67409f1cd6a84724180a8273f5d87b379
ktiwari8760 <110582982+ktiwari8760@users.noreply.github.com>
2026-06-07 15:31:38 +0530

    Add: can-place-flowers - submission-7

---
+++ Data Structures & Algorithms/can-place-flowers/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canPlaceFlowers(self, f: List[int], n: int) -> bool:
+        if len(f) == 1:
+            if f[0] == 0:
+                f[0] = 1
+                n = n-1
+        if len(f) >= 2:
+            if f[0] == 0 and f[1] != 1:
+                f[1] = 1
+                n = n-1
+            if f[-1] == 0 and f[-2] != 1:
+                f[-1] = 1
+                n = n-1
+        for i in range(1 , len(f)-1):
+            if f[i] == 0 and f[i-1] != 1 and f[i+1] != 1:
+                f[i] = 1
+                n = n-1
+        return n <= 0
+                
